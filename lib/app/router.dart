239a23c3d70a5efaeaa67adada0a3101f0733d4b
airy-swift <thanks.xa@gmail.com>
2024-04-07 08:43:20 +0900

    happiness level screen作成

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,13 +1,14 @@
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:practice_flutter/ui/happiness_level/happiness_level_screen.dart';
 
 final routerProvider = Provider(
   (ref) => GoRouter(
     routes: <GoRoute>[
-      // GoRoute(
-      //   path: _Dummy.routePath,
-      //   builder: (_, __) => const _Dummy(),
-      // ),
+      GoRoute(
+        path: HappinessLevelScreen.routePath,
+        builder: (_, __) => const HappinessLevelScreen(),
+      ),
     ],
   ),
 );
